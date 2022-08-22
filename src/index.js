2f83d4ee466bf7cb46f52b8590ca4933c53a07a2
Soyoung Bae <soybae@Soyoungs-MacBook-Pro.local>
2022-08-21 18:29:25 -0700

    installed styled components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App(){
-  return <h1>React Ground Zero</h1>
+  return <h1>React with Styled Components Starter</h1>
 }
 
 root.render(<App/>);
